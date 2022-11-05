4c4447fcd33fd16ec8f9cc81dd5ca6a79c56311e
Alexwell <935562189@qq.com>
2022-11-05 21:45:11 +0100

    Update

--- ColorExtractor/tools.py
+++ ColorExtractor/tools.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
+np.set_printoptions(threshold=np.inf)
 
 # green, yellow, red, dark red
 bg_color = [[148, 200, 97], [225, 131, 49], [211, 45, 31],  [146, 36, 29]]
@@ -29,74 +30,99 @@ def DDA(x1, y1, x2, y2, point_list):
         x += x_inc
         y += y_inc
 
-def gen_Filter(lng_seq, lat_seq, idx, h, w):
+def gen_Filter(max_lat_delta, max_lng_delta, rest_point_delta):
     # cv.line(logo, (lng, 0), (lng, h), (0, 255, 255), thickness=2)
     # cv.line(logo, (0, lat), (w, lat), (255, 0, 0), thickness=2)
         
-    lng = lng_seq[0]
-    lat = lat_seq[0]
+    # lng = lng_seq[0]
+    # lat = lat_seq[0]
     
-    lng = int(h*lng)
-    lat = int(w*lat)
+    # lng = int(h*lng)
+    # lat = int(w*lat)
     
-    rest_point_delta = [(0, 0)]
-    max_lng_delta = 0
-    max_lat_delta = 0
-    for i in range(1, len(lng_seq)):
-        lng_ = int(h*lng_seq[i])
-        lat_ = int(w*lat_seq[i])
-        max_lat_delta = max(max_lat_delta, abs(lat_-lat))
-        max_lng_delta = max(max_lng_delta, abs(lng_-lng))
-        rest_point_delta.append((lat_ - lat, lng_ - lng))
-    rest_point_delta = list(set(rest_point_delta))
+    # rest_point_delta = [(0, 0)]
+    # max_lng_delta = 0
+    # max_lat_delta = 0
+    # for i in range(1, len(lng_seq)):
+    #     lng_ = int(h*lng_seq[i])
+    #     lat_ = int(w*lat_seq[i])
+    #     print(lat_, lng_)
+    #     max_lat_delta = max(max_lat_delta, abs(lat_-lat))
+    #     max_lng_delta = max(max_lng_delta, abs(lng_-lng))
+    #     rest_point_delta.append((lat_ - lat, lng_ - lng))
+    # rest_point_delta = list(set(rest_point_delta))
     max_delta = max(max_lat_delta, max_lng_delta)
     # if max_delta % 2 != 0:
     #     max_delta += 1
-    width = max(max_delta + 1, 12)
-    kernel_size = (width, width)
-    kernel = np.zeros(kernel_size, np.uint8)
-    if max_lat_delta > max_lng_delta:
-        rest_point_delta.sort(key=lambda x: x[0])
-    else:
-        rest_point_delta.sort(key=lambda x: x[1])
+    
+    # direction = -1
+    # if max_lat_delta > max_lng_delta:
+    #     rest_point_delta.sort(key=lambda x: x[0])
+    #     if len(rest_point_delta) != 1:
+    #         dx = rest_point_delta[0][0] - rest_point_delta[1][0]
+    #         if dx < 0:
+    #             direction = 1 # down
+    #         elif dx > 0:
+    #             direction = 2 # up
+    # else:
+    #     rest_point_delta.sort(key=lambda x: x[1])
+    #     print(rest_point_delta)
     # DDA interpolation
+    
     interpolated_point = []
     for i in range(len(rest_point_delta) - 1):
         DDA(rest_point_delta[i][0], rest_point_delta[i][1],
             rest_point_delta[i+1][0], rest_point_delta[i+1][1], interpolated_point)
     interpolated_point.append(rest_point_delta[-1])
+    interpolated_point = list(set(interpolated_point))
+    min_val = 0
+    for i in range(len(rest_point_delta)):
+        min_val = min(min_val, rest_point_delta[i][0], rest_point_delta[i][1])
 
+    for i in range(len(rest_point_delta)):
+        rest_point_delta[i] = (rest_point_delta[i][0] - min_val, rest_point_delta[i][1] - min_val)
     
+    interpolated_point = []
+    print("rest_point_delta")
+    print(rest_point_delta)
+    for i in range(len(rest_point_delta) - 1):
+        DDA(rest_point_delta[i][0], rest_point_delta[i][1],
+            rest_point_delta[i+1][0], rest_point_delta[i+1][1], interpolated_point)
+
+    width = max(max_delta + 1 - min_val, 12)
+    kernel_size = (width, width)
+    kernel = np.zeros(kernel_size, np.uint8)
     for point in interpolated_point:
-        if max_lat_delta > max_lng_delta:
-            kernel[point[0]][point[1]+max_lat_delta//2] = 1
+        kernel[point[0], point[1]] = 1
+    # delet all zero row
+    kernel = kernel[~np.all(kernel == 0, axis=1)]
+    # delet all zero col
+    kernel = kernel[:, ~np.all(kernel == 0, axis=0)]
 
-            # # Extend the range
-            # delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-            # for d in delta:
-            #     if 0 <= point[0]+d[0] < max_delta+1 and 0 <= point[1]+d[1]+max_lat_delta//2 < max_delta+1:
-            #         kernel[point[0]+d[0]][point[1]+max_lat_delta//2+d[1]] = max(kernel[point[0]+d[0]][point[1]+max_lat_delta//2+d[1]], 1)
-            
-            # delta = [(2, 0), (-2, 0), (0, 2), (0, -2)]
-            # for d in delta:
-            #     if 0 <= point[0]+d[0] < max_delta+1 and 0 <= point[1]+d[1]+max_lat_delta//2 < max_delta+1:
-            #         kernel[point[0]+d[0]][point[1]+max_lat_delta//2+d[1]] = max(kernel[point[0]+d[0]][point[1]+max_lat_delta//2+d[1]], 1)
-            
-        else:
-            
-            kernel[point[0]+max_lng_delta//2][point[1]] = 1  
-
-            # # Extend the range
-            # delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-            # for d in delta:
-            #     if 0 <= point[0]+d[0]+max_lng_delta//2 < max_delta+1 and 0 <= point[1]+d[1] < max_delta+1:
-            #         kernel[point[0]+max_lng_delta//2+d[0]][point[1]+d[1]] = max(kernel[point[0]+max_lng_delta//2+d[0]][point[1]+d[1]], 1)
-            
-            # delta = [(2, 0), (-2, 0), (0, 2), (0, -2)]
-            # for d in delta:
-            #     if 0 <= point[0]+d[0]+max_lng_delta//2 < max_delta+1 and 0 <= point[1]+d[1] < max_delta+1:
-            #         kernel[point[0]+max_lng_delta//2+d[0]][point[1]+d[1]] = max(kernel[point[0]+max_lng_delta//2+d[0]][point[1]+d[1]], 1)
+    # padding to same size
+    x_len = kernel.shape[0]
+    y_len = kernel.shape[1]
     
+    if x_len > y_len:
+        # padding y equally both side
+        padding = (x_len - y_len) // 2
+        kernel = np.pad(kernel, ((0, 0), (padding, padding)), 'constant')
+    elif x_len < y_len:
+        # padding x equally both side
+        padding = (y_len - x_len) // 2
+        kernel = np.pad(kernel, ((padding, padding), (0, 0)), 'constant')
+
+    x_len = kernel.shape[0]
+    y_len = kernel.shape[1]
+    if x_len != y_len:
+        if x_len < y_len:
+            # delete one col
+            kernel = kernel[:, 1:]
+        else:
+            # delete one row
+            kernel = kernel[1:, :]
+    print(kernel.shape)
+    print(kernel)
     return kernel
 
 
@@ -133,35 +159,74 @@ def image_process(lng: float, lat: float, idx, output=None):
     except:
         return -1
 
-    # import torch.nn.functional as F
-def image_process_position_seq(lng_seq, lat_seq, idx, output=None):
+
+def checkRoadType(mask):
+    pass
+
+def point_delta(lng_seq, lat_seq, idx):
+    logo = cv.imread('./output/8756_8768_12124_12137_z15_t' +
+                         str(idx*600)+'.png')
+    logo = cv.cvtColor(logo, cv.COLOR_BGR2RGB)
+    h, w = logo.shape[0:2]
+        
+    lng = lng_seq[0]
+    lat = lat_seq[0]
+    
+    lng = int(h*lng)
+    lat = int(w*lat)
+
+    rest_point_delta = [(0, 0)]
+    max_delta = 0
+    max_lat_delta = 0
+    max_lng_delta = 0
+    for i in range(1, len(lng_seq)):
+        lng_ = int(h*lng_seq[i]) # x
+        lat_ = int(w*lat_seq[i]) # y
+        max_lat_delta = max(max_lat_delta, abs(lat_-lat))
+        max_lng_delta = max(max_lng_delta, abs(lng_-lng))
+        max_delta = max(max_delta, max(max_lat_delta, max_lng_delta))
+        if max_delta < 100:
+            rest_point_delta.append((lng_-lng, lat_-lat))
+    rest_point_delta = set(rest_point_delta)
+    rest_point_delta = list(rest_point_delta)
+    return rest_point_delta, max_delta
+    
+def image_process_position_seq(lng_seq, lat_seq, idx, rest_point_delta, max_delta,  output=None):
     # cv.line(logo, (lng, 0), (lng, h), (0, 255, 255), thickness=2)
     # cv.line(logo, (0, lat), (w, lat), (255, 0, 0), thickness=2)
     try:
         logo = cv.imread('./output/8756_8768_12124_12137_z15_t' +
                          str(idx*600)+'.png')
         logo = cv.cvtColor(logo, cv.COLOR_BGR2RGB)
-        h, w = logo.shape[0:2]
         
+        h, w = logo.shape[0:2]
+            
         lng = lng_seq[0]
         lat = lat_seq[0]
-    
+        
         lng = int(h*lng)
         lat = int(w*lat)
-        
+            
         print("First point: ", lng, lat)
         rest_point_delta = [(0, 0)]
         max_delta = 0
+        max_lat_delta = 0
+        max_lng_delta = 0
         for i in range(1, len(lng_seq)):
-            lng_ = int(h*lng_seq[i])
-            lat_ = int(w*lat_seq[i])
-            max_delta = max(max_delta, max(abs(lng_-lng), abs(lat_-lat)))
-            rest_point_delta.append((lat_ - lat, lng_ - lng))
+            lng_ = int(h*lng_seq[i]) # x
+            lat_ = int(w*lat_seq[i]) # y
+            max_lat_delta = max(max_lat_delta, abs(lat_-lat))
+            max_lng_delta = max(max_lng_delta, abs(lng_-lng))
+            max_delta = max(max_delta, max(max_lat_delta, max_lng_delta))
+            if max_delta < 100:
+                rest_point_delta.append((lng_-lng, lat_-lat))
         rest_point_delta = set(rest_point_delta)
+        rest_point_delta = list(rest_point_delta)
+
         print("point delta: ", rest_point_delta)
         print("Max delta: ", max_delta)
         
-        img_half_width = max(max_delta + 1, 12)
+        img_half_width = max(max_delta + 1, 50)
         point_on_map = np.zeros((2*img_half_width, 2*img_half_width))
         for point in rest_point_delta:
             point_on_map[point[0]+img_half_width][point[1]+img_half_width] = 1
@@ -169,7 +234,7 @@ def image_process_position_seq(lng_seq, lat_seq, idx, output=None):
                        max(0, lng-img_half_width): min(h, lng+img_half_width)]  # [y0:y1, x0:x1]
         
         # Generate filter kernel
-        kernel = gen_Filter(lng_seq, lat_seq, idx, h, w)
+        kernel = gen_Filter(max_lat_delta, max_lng_delta, rest_point_delta[:])
         # Extract road
         cropped_road = np.zeros((cropped.shape[0], cropped.shape[1]))
         for i in range(cropped.shape[0]):
@@ -183,23 +248,37 @@ def image_process_position_seq(lng_seq, lat_seq, idx, output=None):
                         break
         
         # Apply filter
-        
         print("Kernel size: ", kernel.shape)
         print("Image size: ", cropped_road.shape)
-        # output = convolve2D(cropped_road, kernel, padding=paddle_size)
 
-        output = cv.filter2D(cropped_road, 1, kernel)
-        # output = F.conv2d(cropped_road, kernel)
-        threshold = int(kernel.sum() * 0.8)
+        output = cv.filter2D(cropped_road, 1, kernel) # Ouptput is the mask of intrerested area
+        threshold = int(kernel.sum() * 0.6)
+
+        # Without interest area
         output[output < threshold] = 0
         output[output >= threshold] = 1
+        
+        # # With interest area
+        # detect_range = 0
+        # for i in range(len(output)):
+        #     for j in range(len(output[0])):
+        #         if output[i][j] >= threshold and abs(i - img_half_width) < max_delta + detect_range and abs(j - img_half_width) < max_delta + detect_range:
+        #             output[i][j] = 1
+        #         else:
+        #             output[i][j] = 0
+
+        # Todo: Determine the type of road
+
+
+        # Todo: Determine which direction the road is going
+        
 
         # Center crop to 24*24
         center_crop_output = output[img_half_width-12:img_half_width+12, img_half_width-12:img_half_width+12]
         center_crop_image = cropped[img_half_width-12:img_half_width+12, img_half_width-12:img_half_width+12]
         
         # Mix the point on map with the cropped image
-        fig = plt.figure(figsize=[6,6])
+        fig = plt.figure(figsize=[10,10])
         ax = fig.add_subplot(121)
         ax.imshow(cropped)
         ax.imshow(output, alpha=0.5)
